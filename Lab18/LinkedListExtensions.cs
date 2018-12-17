93670a5603001494396b4ce642f1ada7689c00eb
James Jackson <James@Grandcircus.co>
2018-12-16 21:55:20 -0500

    James TA Review

--- Lab18/LinkedListExtensions.cs
+++ Lab18/LinkedListExtensions.cs
@@ -1,65 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab18
 {
-    public static class LinkedListExtensions
-    {
-        public static bool RemoveAt<t>(this LinkedList<t> list, int index)
-        {
-            index = index - 1;
-            LinkedListNode<t> currentNode = list.First;
-            for (int i = 0; i <= index && currentNode != null; i++)
-            {
-                if (i != index)
-                {
-                    currentNode = currentNode.Next;
-                    continue;
-                }
-                list.Remove(currentNode);
-            }
-            return false;
-        }
+	// James -- I like that this is all in it's own file.
+	public static class LinkedListExtensions
+	{
+		public static bool RemoveAt<t>(this LinkedList<t> list, int index)
+		{
+			index = index - 1;
+			LinkedListNode<t> currentNode = list.First;
+			for (int i = 0; i <= index && currentNode != null; i++)
+			{
+				if (i != index)
+				{
+					currentNode = currentNode.Next;
+					continue;
+				}
+				list.Remove(currentNode);
+			}
+			return false;
+		}
 
-        public static void PrintReverse<t>(this LinkedList<t> list)
-        {
-            for (int i = list.Count - 1; i >= 0; i--)
-            {
-                Console.WriteLine(list.ElementAt(i));
-            }
-        }
+		public static void PrintReverse<t>(this LinkedList<t> list)
+		{
+			for (int i = list.Count - 1; i >= 0; i--)
+			{
+				Console.WriteLine(list.ElementAt(i));
+			}
+		}
 
-        public static bool InsertAt<t>(this LinkedList<t> list, int index, t o)
-        {
-            index = index - 1;
-            LinkedListNode<t> currentNode = list.First;
-            for (int i = 0; i <= index && currentNode != null; i++)
-            {
-                if (i != index)
-                {
-                    currentNode = currentNode.Next;
-                    continue;
-                }
+		public static bool InsertAt<t>(this LinkedList<t> list, int index, t o)
+		{
+			index = index - 1;
+			LinkedListNode<t> currentNode = list.First;
+			for (int i = 0; i <= index && currentNode != null; i++)
+			{
+				if (i != index)
+				{
+					currentNode = currentNode.Next;
+					continue;
+				}
 
-                list.AddAfter(currentNode, o);
+				list.AddAfter(currentNode, o);
 
-            }
+			}
 
-            return false;
-        }
+			return false;
+		}
 
-        public static void WriteOut<t>(this LinkedList<t> list)
-        {
-            foreach (var node in list)
-            {
-                Console.WriteLine(node);
-            }
-        }
+		public static void WriteOut<t>(this LinkedList<t> list)
+		{
+			foreach (var node in list)
+			{
+				Console.WriteLine(node);
+			}
+		}
 
-       
 
-    }
+
+	}
 }
